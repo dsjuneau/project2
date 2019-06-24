8f76fefde96a68f2b4d943791262456369fcfbf0
Scott Juneau <dsjuneau@gmail.com>
2019-06-24 08:09:24 -0500

    Refactored index.js file and removed all unnecessary code

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,8 @@
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-
+//Variables
 var $teamName = $("#teamName");
-var $submitBtn = $("#submit");
-
 const cursor = document.querySelector(".cursor");
 
+//Event listener for moouse move effect
 document.addEventListener("mousemove", e => {
   cursor.setAttribute(
     "style",
@@ -15,6 +10,9 @@ document.addEventListener("mousemove", e => {
   );
 });
 
+//Event listener for Modal
+//Sends team name to the database and receives back the team ID
+//Then stores the ID in local storage for later use
 $("#startGame").on("click", e => {
   e.preventDefault();
   var teamName = {
@@ -38,42 +36,3 @@ $("#startGame").on("click", e => {
   console.log("in here now");
   //window.location.href = "/quiz";
 });
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
